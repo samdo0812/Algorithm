f3400b617f3070c47ed7d1365bb2faa69ccbc570
samdo0812 <39455589+samdo0812@users.noreply.github.com>
2019-01-28 16:42:26 +0900

    test
    test

---
+++ Dovelet(www.dovelet)/Dovelet/Dovelet/swap.cpp
@@ -0,0 +1,19 @@
+#include <stdio.h>
+void swap(int *a , int *b);
+
+int main() {
+	int a, b; 
+	scanf("%d %d" , &a, &b);
+	
+	swap(&a,&b);
+	printf("%d %d" , a,b);
+}
+
+void swap(int *a, int *b) {
+
+	int tmp;
+
+	tmp = *a;
+	*a = *b;
+	*b = tmp;
+}
