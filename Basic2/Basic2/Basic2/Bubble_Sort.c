5ce8ccab7eac1adf26f5b437ec186f3927842c25
samdo0812 <39455589+samdo0812@users.noreply.github.com>
2019-02-06 19:51:28 +0900

    c
    c

---
+++ Basic2/Basic2/Basic2/Bubble_Sort.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+/*
+	한번 반복 할 때 마다 집합의 크기가 1 줄어듦
+	10 + 9 + 8 + 7 + ... + 1
+	= > N * (N+1) /2
+	= >O(N*N)
+	= >O(N^2)
+	실제 수행시간은 선택 정렬보다 더 느림
+*/
+
+int main() {
+
+	int array[10] = { 1,10,5,8,7,6,4,3,2,9 };
+	int temp;
+
+	for (int i = 0; i < 10; i++)
+	{
+		for (int j = 0; j < 9 - i; j++)
+		{
+			if (array[j] > array[j + 1])
+			{
+				temp = array[j];
+				array[j] = array[j + 1];
+				array[j + 1] = temp;
+			}
+		}
+	}
+
+	for (int i = 0; i < 10; i++)
+	{
+		printf("%d ", array[i]);
+	}
+}
---
+++ Basic2/Basic2/Basic2/Insertion_Sort.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+/*
+	10 + 9 + 8 + ... + 1
+	= > N * (N + 1) / 2
+	= > N * N
+	= > O(N*N)
+	= > O(N^2)
+	거의 정렬되어 있는 경우에는 굉장히 효율적
+*/
+
+int main() {
+	int array[10] = { 1,10,5,8,7,6,4,3,2,9 };
+	int j, temp;
+
+	for (int i = 0; i < 9; i++)
+	{
+		j = i;
+		while (j >=  0 && array[j] > array[j+1])
+		{
+			temp = array[j];
+			array[j] = array[j + 1];
+			array[j + 1] = temp;
+			j--;
+		}
+	}
+
+	for (int i = 0; i < 10; i++)
+	{
+		printf("%d ", array[i]);
+	}
+}
