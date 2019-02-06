3bd531bffad8b798ba9ebd4e1b7340f0e6b3039e
samdo0812 <39455589+samdo0812@users.noreply.github.com>
2019-02-06 21:20:53 +0900

    Quick_Sort
    Quick_Sort

--- Basic2/Basic2/Basic2/Quick_Sort.c
+++ Basic2/Basic2/Basic2/Quick_Sort.c
@@ -18,50 +18,53 @@ Quick Sort
 int number = 10;
 int data[10] = {1,10,5,8,7,6,4,3,2,9};
 
+void show(){
+	int i;
+	for (i = 0; i < number; i++)
+	{
+		printf("%d ", data[i]);
+	}
+}
+
 void quickSort(int *data, int start, int end) {
-	if (start >= end) //원소가 1개인 경우
+	if (start >= end)	//원소가 1개인 경우 그대로 두기
 	{
 		return;
 	}
 
-	int key = start; //키는 첫번째 원소 (피봇)
-	int i = start + 1;
-	int j = end;
-	int temp;
+	int key = start;	//키는 첫번째 원소
+	int i = start + 1, j = end, temp;
 
-	while (i<=j)	//엇갈릴 때까지 반복
+	while (i <= j)	//엇갈릴 때까지 반복
 	{
-		while (data[i] <= data[key])	//키 값보다 큰 값을 만날 때 까지
+		while (i<=end && data[i] <= data[key])	//키 값보다 큰 값을 만날 때 까지
 		{
 			i++;
 		}
-		while (data[j] >= data[key] && j > start)	//키 값보다 작은 값을 만날 때 까지
+		while (j> start && data[j] >= data[key]) //키 값보다 작은 값을 만날 때
 		{
 			j--;
 		}
-		if (i > j)	//현재 엇갈린 상태면 키 값과 교체
+
+		if (i>j)	//현재 엇갈린 상태면 키 값과 교체
 		{
 			temp = data[j];
 			data[j] = data[key];
 			data[key] = temp;
 		}
-		else
+		else	//엇갈리지 않았다면 i와 j를 교체
 		{
-			temp = data[j];
-			data[j] = data[i];
-			data[i] = temp;
+			temp = data[i];
+			data[i] = data[j];
+			data[j] = temp;
 		}
 	}
 
 	quickSort(data, start, j - 1);
 	quickSort(data, j + 1, end);
-
 }
 
 int main() {
 	quickSort(data, 0, number - 1);
-	for (int i = 0; i < number; i++)
-	{
-		printf("%d " , data[i]);
-	}
+	show();
 }
