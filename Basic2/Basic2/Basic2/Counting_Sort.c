057710c50396d471a8e78c235e18708e20b1506b
samdo0812 <39455589+samdo0812@users.noreply.github.com>
2019-02-20 21:18:14 +0900

    Counting Sort
    Counting Sort

--- Basic2/Basic2/Basic2/Counting_Sort.c
+++ Basic2/Basic2/Basic2/Counting_Sort.c
@@ -7,16 +7,19 @@ int main() {
 					 3,4,4,3,5,1,2,3,5,2,
 					 3,1,4,3,5,1,2,1,1,1,};
 
+	//count 배열 초기화
 	for (int i = 0; i <= 5; i++)
 	{
 		count[i] = 0;
 	}
 
+	//count 배열의 현재 요소값들이 0, array의 요소들을 확인 후 count의 요소값들을 ++ 해줌
 	for (int i = 0; i < 30; i++)
 	{
 		count[array[i]]++;
 	}
-
+	
+	//출력
 	for (int i = 0; i <= 5; i++)
 	{
 		if (count[i] != 0)
